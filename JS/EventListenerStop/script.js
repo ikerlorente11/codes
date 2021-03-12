32d3e209a49325d49b9da72b9d762fbdb0a06e31
ikerlorente11 <ikerlorentecalvo@gmail.com>
2021-03-12 15:02:26 +0100

    EventListenerStop code documentation

--- JS/EventListenerStop/script.js
+++ JS/EventListenerStop/script.js
@@ -1,18 +1,21 @@
+//Get elements
 const grandparent = document.querySelector(".grandparent")
 const parent = document.querySelector(".parent")
 const child = document.querySelector(".child")
 
+//Set click event listener to grandparent
 grandparent.addEventListener('click', e => {
-    e.stopPropagation()
     console.log("I'm the grandparent")
 })
 
+//Set click event listener to parent
+//Set stop propagation to not execute grandparent listener on clicking in the parent or the child
 parent.addEventListener('click', e => {
     e.stopPropagation()
     console.log("I'm the parent")
 })
 
+//Set click event listener to child
 child.addEventListener('click', e => {
-    e.stopPropagation()
     console.log("I'm the child")
 })
