2fecfbd1ad3fbffb0bf6397b942749843c7dd2d7
ikerlorente11 <ikerlorentecalvo@gmail.com>
2021-04-04 22:12:08 +0200

    PopUp commented

--- JS/PopUp/script.js
+++ JS/PopUp/script.js
@@ -4,31 +4,49 @@ const overlay = document.getElementById('overlay');
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', () =>{
+        //Get the element with the id of the target we define
         const modal = document.querySelector(button.dataset.modalTarget)
+        //Shows selected element
         openModal(modal)
     })
 })
 
 overlay.addEventListener('click', () => {
+    //Get elements with the class modal and active
     const modals = document.querySelectorAll('.modal.active')
     modals.forEach(modal => {
+        //Hide selected elements
         closeModal(modal)
     })
 })
 
 closeModalButtons.forEach(button => {
     button.addEventListener('click', () =>{
+        //Get the closest element with the class modal above clicked button
         const modal = button.closest('.modal')
+        //Hide selected element
         closeModal(modal)
     })
 })
 
+/**
+ * Shows the PopUp
+ * 
+ * @param {Element} modal The container we want to show
+ * @returns If modal is null it returns nothing
+ */
 function openModal(modal){
     if(modal == null) return
     modal.classList.add('active')
     overlay.classList.add('active')
 }
 
+/**
+ * Hides the PopUp
+ * 
+ * @param {Element} modal The container we want to hide
+ * @returns If modal is null it returns nothing
+ */
 function closeModal(modal){
     if(modal == null) return
     modal.classList.remove('active')
