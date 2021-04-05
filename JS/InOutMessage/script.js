fafa7fb94607ef7a9e75c6b6f1c649c17b12a5b7
ikerlorente11 <ikerlorentecalvo@gmail.com>
2021-04-05 14:16:02 +0200

    InOutMessage updated for all positions messages

--- JS/InOutMessage/script.js
+++ JS/InOutMessage/script.js
@@ -1,13 +1,17 @@
 //Get elements
 const container = document.querySelector('.container');
-const message = document.querySelector('.message');
+const messages = document.querySelectorAll('.message');
 
-//Add faceinout class to element we want to activate de message
+//Add faceinout class to elements we want to activate de message
 container.addEventListener('click', () =>{
-    message.classList.add('faceinout');
+    messages.forEach(message => {
+        message.classList.add('faceinout');
+    })
 })
 
 //Remove faceinout class when animation ends
-message.addEventListener('animationend', () => {
-    message.classList.remove('faceinout');
+messages.forEach(message => {
+    message.addEventListener('animationend', () => {
+        message.classList.remove('faceinout');
+    })
 })
