9096fd135ab00002f549744bc26ecc2d37c29d2b
ikerlorente11 <ikerlorentecalvo@gmail.com>
2022-03-28 16:31:14 +0200

    Complete web layout implementation

---
+++ WebDevelopment/Layout/CompleteWeb/script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".menu_icon").forEach(element => {
+    element.addEventListener('click', function() {
+
+        if(document.documentElement.clientWidth > 992){
+            document.getElementById("body").classList.toggle("body_close")
+            document.getElementById("menu").classList.toggle("menu_close")
+            document.querySelectorAll(".menu_icon_out")[0].classList.toggle("d-visible")
+        }else{
+            document.getElementById("menu").classList.toggle("menu_open")
+            document.getElementById("body").classList.remove("body_close")
+            document.getElementById("menu").classList.remove("menu_close")
+            document.querySelectorAll(".menu_icon_out")[0].classList.remove("d-visible")
+        }
+    })
+})
